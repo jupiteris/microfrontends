a174377c52f567eefed97bdeda55e0acabe3150d
jupiteris <jupiter.programmer@gmail.com>
2023-02-15 14:53:34 -0800

    hi

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default function () {
     return (
         <div>
-            <h1>Hi There!!!!</h1>
+            <h1>Hi There</h1>
             <hr />
             <MarketingApp />
         </div>
--- marketing/src/components/Landing.js
+++ marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              Landing Page
+              Home Page
             </Typography>
             <Typography
               variant="h5"
