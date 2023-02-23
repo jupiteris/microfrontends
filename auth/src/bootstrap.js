6ef3462f1af299732e05c88e1e52cb56092fbbbd
jupiteris <jupiter.programmer@gmail.com>
2023-02-23 10:03:38 -0800

    authentication flow

--- auth/src/bootstrap.js
+++ auth/src/bootstrap.js
@@ -3,14 +3,14 @@ import ReactDom from 'react-dom';
 import App from './App';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (el, { onNavigate, onSignIn, defaultHistory, initialPath }) => {
     const history = defaultHistory || createMemoryHistory({
         initialEntries: [initialPath]
     });
     if (onNavigate) {
         history.listen(onNavigate)
     }
-    ReactDom.render(<App history={history} />, el);
+    ReactDom.render(<App history={history} onSignIn={onSignIn} />, el);
 
     return {
         onParentNavigate({ pathname: nextPathname }) {
--- container/src/App.js
+++ container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import Header from './components/Header';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles'
@@ -10,15 +10,19 @@ const generateClassName = createGenerateClassName({
     productionPrefix: 'co'
 })
 
-export default function () {
+export default function ({ }) {
+    const [signedIn, setSignedIn] = useState(false);
+
     return (
         <BrowserRouter>
             <StylesProvider generateClassName={generateClassName}>
                 <div>
-                    <Header />
+                    <Header signedIn={signedIn} onSignOut={() => setSignedIn(false)} />
                     <Suspense fallback={<div>loading...</div>}>
                         <Switch>
-                            <Route path='/auth' component={AuthAppLazy} />
+                            <Route path='/auth'>
+                                <AuthAppLazy onSignIn={() => setSignedIn(true)} />
+                            </Route>
                             <Route path='/' component={MarketingAppLazy} />
                         </Switch>
                     </Suspense>
